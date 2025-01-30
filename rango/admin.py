cfa4ef02034cd8b5893f9a04d033912767de1eef
“Mohammed <msm_alsenani@icloud.com>
2025-01-30 22:17:19 +0000

    chapter 5 done with testing imp

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
-# Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')  # Fields to display in the list view
+    list_filter = ('category',)  # adding a filter option by category
+    search_fields = ('title', 'url')  # allowing searching by title and URL
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin) # Register the Category and Page models with the customized admin interface
+
